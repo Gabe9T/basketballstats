74ecaa2bb27c36624eeaad4d95dbdd30f2d0233c
gabe <gabrieltucker260@gmail.com>
2024-04-28 16:43:15 -0700

    for firebase

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from bs4 import BeautifulSoup
 import requests
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("./serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
 
