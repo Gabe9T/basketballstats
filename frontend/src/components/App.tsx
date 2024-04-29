8ca8e1ef1612d5780357018b2dfbb692040d4624
gabe <gabrieltucker260@gmail.com>
2024-04-29 13:29:19 -0700

    made starter frontend

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.tsx
+import React from 'react';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
-function App() {
-  const [count, setCount] = useState(0)
+const firebaseConfig = {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+};
+
+// Initialize Firebase app
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 }
 
-export default App
+const App: React.FC = () => {
+  const firestore = firebase.firestore();
+  const playersRef = firestore.collectionGroup('players');
+  const [players, loading, error] = useCollectionData(playersRef, { idField: 'id' });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Basketball Players Stats</h1>
+      <div>
+        {players && players.map((player: any) => (
+          <div key={player.name}>
+            <h2>{player.name.toUpperCase()}</h2>
+            <ul>
+              {Object.keys(player).map((key: string) => {
+
+                return (
+                  <li key={key}>
+                    <strong>{key}</strong>: {player[key]}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
