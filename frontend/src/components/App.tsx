cc9b79267e91c0f6e72d691af5f82c494d0e8ee3
gabe <gabrieltucker260@gmail.com>
2024-04-30 10:37:35 -0700

    added search queries for stats

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -7,7 +7,6 @@ const firebaseConfig = {
 
 };
 
-// Initialize Firebase app
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
@@ -15,49 +14,113 @@ if (!firebase.apps.length) {
 const App: React.FC = () => {
   const firestore = firebase.firestore();
   const playersRef = firestore.collectionGroup('players');
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTermName, setSearchTermName] = useState('');
+  const [searchTermYearsActive, setSearchTermYearsActive] = useState('');
+  const [searchTermPosition, setSearchTermPosition] = useState('');
+  const [searchTermHeight, setSearchTermHeight] = useState('');
+  const [searchTermWeight, setSearchTermWeight] = useState('');
+  const [searchTermDOB, setSearchTermDOB] = useState('');
+  const [searchTermCollege, setSearchTermCollege] = useState('');
   const [players, setPlayers] = useState<any[]>([]);
 
-  // Load players from local storage on component mount
   useEffect(() => {
     const localPlayers = localStorage.getItem('basketballPlayers');
     if (localPlayers) {
       setPlayers(JSON.parse(localPlayers));
+    } else {
+      //grabs data from db if not already cached
+      fetchPlayersFromFirestore();
     }
   }, []);
 
-  // Fetch players from Firestore and update local storage
-  const [data] = useCollectionData(playersRef, { idField: 'id' });
-  useEffect(() => {
+  const fetchPlayersFromFirestore = () => {
+    const [data] = useCollectionData(playersRef, { idField: 'id' });
     if (data) {
       localStorage.setItem('basketballPlayers', JSON.stringify(data));
       setPlayers(data);
     }
-  }, [data]);
+  };
 
-  const filteredPlayers = players?.filter((player: any) =>
-    player.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const filteredPlayers = players?.filter((player: any) => {
+    const startYear = parseInt(player['0'], 10);
+    const endYear = parseInt(player['1'], 10);
+    const searchYear = parseInt(searchTermYearsActive, 10);
+    
+    return (
+      (searchTermName === '' || player.name.toLowerCase().includes(searchTermName.toLowerCase())) &&
+      (searchTermYearsActive === '' || (startYear <= searchYear && searchYear <= endYear)) &&
+      (searchTermPosition === '' || player['2'].toLowerCase() === searchTermPosition.toLowerCase()) &&
+      (searchTermHeight === '' || player['3'].toLowerCase().includes(searchTermHeight.toLowerCase())) &&
+      (searchTermWeight === '' || player['4'].toLowerCase().includes(searchTermWeight.toLowerCase())) &&
+      (searchTermDOB === '' || player['5'].toLowerCase().includes(searchTermDOB.toLowerCase())) &&
+      (searchTermCollege === '' || player['7'].toLowerCase().includes(searchTermCollege.toLowerCase()))
+    );
+  });
+
+  const positions: string[] = Array.from(new Set(players?.map(player => player['2'].toLowerCase()))) || [];
 
   return (
     <div>
       <h1>Basketball Players Stats</h1>
-      <input
-        type="text"
-        placeholder="Search by name"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+      <div>
+        <input
+          type="text"
+          placeholder="Search by name"
+          value={searchTermName}
+          onChange={(e) => setSearchTermName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Search by years active"
+          value={searchTermYearsActive}
+          onChange={(e) => setSearchTermYearsActive(e.target.value)}
+        />
+        <select
+          value={searchTermPosition}
+          onChange={(e) => setSearchTermPosition(e.target.value)}
+        >
+          <option value="">Select Position</option>
+          {positions.map((position, index) => (
+            <option key={index} value={position}>{position.toUpperCase()}</option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Search by height"
+          value={searchTermHeight}
+          onChange={(e) => setSearchTermHeight(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Search by weight"
+          value={searchTermWeight}
+          onChange={(e) => setSearchTermWeight(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Search by DOB"
+          value={searchTermDOB}
+          onChange={(e) => setSearchTermDOB(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Search by college"
+          value={searchTermCollege}
+          onChange={(e) => setSearchTermCollege(e.target.value)}
+        />
+      </div>
       <div>
         {filteredPlayers?.map((player: any) => (
           <div key={player.name}>
             <h2>{player.name.toUpperCase()}</h2>
             <ul>
-              {Object.keys(player).map((key: string) => (
-                <li key={key}>
-                  <strong>{key}</strong>: {player[key]}
-                </li>
-              ))}
+              <li>Start Year: {player['0']}</li>
+              <li>End Year: {player['1']}</li>
+              <li>Position: {player['2']}</li>
+              <li>Height: {player['3']}</li>
+              <li>Weight: {player['4']}</li>
+              <li>DOB: {player['5']}</li>
+              <li>College: {player['7']}</li>
             </ul>
           </div>
         ))}
